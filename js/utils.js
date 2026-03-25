75af5a643dc117b300351a10c9e1f254ad46e73e
WISROVI <wisrovi@debian.com>
2026-03-26 00:14:12 +0100

    Add debounce, skeleton loaders, and better loading states

---
+++ js/utils.js
@@ -0,0 +1,22 @@
+export function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+export function throttle(func, limit) {
+  let inThrottle;
+  return function executedFunction(...args) {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+}
